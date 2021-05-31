4b454f10d2302c037ae99729b29769d59de292cf
Lilith Vox <lilyvluce@outlook.com>
2021-05-31 14:15:50 -0700

    finished the theaters route

--- src/theaters/theaters.controller.js
+++ src/theaters/theaters.controller.js
@@ -1,8 +1,19 @@
 const service = require('./theaters.service');
+const reduceProps = require('../utils/reduce-properties');
+const addMovies = reduceProps('theater_id',{
+  'movie_id':['movies', null, 'movie_id'],
+  'title':['movies', null, 'title'],
+  'runtime_in_minutes':['movies', null, 'runtime_in_minutes'],
+  'rating':['movies', null, 'rating'],
+  'description':['movies', null, 'description'],
+  'image_url':['movies', null,'image_url'],
+  'is_showing':['movies', null, 'is_showing'],
+  'theater_id':['movies', null, 'theater_id'],
+})
 
 async function list(req, res) {
   const theaters = await service.list();
-  res.status(200).json({ data: theaters });
+  res.status(200).json({ data: addMovies(theaters) });
 }
 
 module.exports = {
--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -1,7 +1,10 @@
 const knex = require('../db/connection');
 
-function list(){
-  return knex('theaters').select('*');
+function list() {
+  return knex('theaters as t')
+    .select('t.*', 'm.*')
+    .innerJoin('movies_theaters as mt', 't.theater_id', 'mt.theater_id')
+    .leftJoin('movies as m', 'mt.movie_id', 'm.movie_id');
 }
 
 module.exports = {
